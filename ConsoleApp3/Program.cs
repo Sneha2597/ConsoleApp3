d5eec865808bb81f66cd2932200260b13852e8f8
Sneha Aandal Mutha <srisneha1230@gmail.com>
2020-07-28 16:43:25 +0530

    Added the new line to merge action

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp3
             Console.WriteLine("Feature started");
             Console.WriteLine("Test the feature of pull request after adding the Branch protection rule in Github");
             Console.WriteLine("Test the Github Actions");
+            Console.WriteLine("Test the Merge action");
         }
     }
 }
