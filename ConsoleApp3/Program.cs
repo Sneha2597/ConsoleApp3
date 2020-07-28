d6008a123ad9053733bf52be9b931e6aefa75f3e
Sneha Aandal Mutha <srisneha1230@gmail.com>
2020-07-28 16:15:05 +0530

    Commited new Line

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp3
             Console.WriteLine("Start Feature");
             Console.WriteLine("Feature started");
             Console.WriteLine("Test the feature of pull request after adding the Branch protection rule in Github");
+            Console.WriteLine("Test the Github Actions");
         }
     }
 }
